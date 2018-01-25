a59c70d46f25b594bf453f8b6b5b47f82fa051b0
Monica Bumb <monicab@dclcorp.local>
2018-01-24 18:26:40 -0800

    Add updated homework

---
+++ spotify-keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+var spotifyKeys = {
+  consumer_key: 'x3tbyvqQWPojW0P5nHQtvIrks',
+  consumer_secret: '3CD79ZgY3iYGNcjHpXglDaesCuKY3bwVn9oFyKNAm6GgyC7rWm',
+  access_token_key: '953467136411803648-2F0jNYKlv3IXJQO8PMVdzc7euqZ9Poe',
+  access_token_secret: 'gsTe55TRDVoEFdPHO55m0kb7wkAngdfL5zbG0tswchK9T',
+  Client ID: '48903d5e089649deb9dd15f8d4ba29f5',
+  Client Secret: '678f2977671f4bc7a631e5b68d16a',
+}
+
+module.exports = spotifyKeys;
---
+++ twitter-keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'x3tbyvqQWPojW0P5nHQtvIrks',
+  consumer_secret: '3CD79ZgY3iYGNcjHpXglDaesCuKY3bwVn9oFyKNAm6GgyC7rWm',
+  access_token_key: '953467136411803648-2F0jNYKlv3IXJQO8PMVdzc7euqZ9Poe',
+  access_token_secret: 'gsTe55TRDVoEFdPHO55m0kb7wkAngdfL5zbG0tswchK9T',
+}
+
+module.exports = twitterKeys;
